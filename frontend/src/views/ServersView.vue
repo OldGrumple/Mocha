8c41a41ffe6fc08f123bbbbdb71f9b1e46114258
Ben Lack <benlack@Bens-MacBook-Air.local>
2025-04-03 17:50:24 +0100

    testing removing node selection

--- frontend/src/views/ServersView.vue
+++ frontend/src/views/ServersView.vue
@@ -122,15 +122,15 @@
           </div>
           <div class="mb-4">
             <label class="block text-sm font-medium text-gray-700">Node</label>
-            <select
+            <!-- <select
               v-model="newServer.nodeId"
               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               required
             >
-              <!-- <option v-for="node in nodes" :key="node._id" :value="node._id">
+              <option v-for="node in nodes" :key="node._id" :value="node._id">
                 {{ node.name }}
-              </option> -->
-            </select>
+              </option>
+            </select> -->
           </div>
           <div class="mb-4">
             <label class="block text-sm font-medium text-gray-700 mb-2">Minecraft Configuration</label>
