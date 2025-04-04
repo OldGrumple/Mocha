ef1ecc5dd823f6e9357065186c4d158ab16c9e6c
Ben Lack <benlack@Bens-MacBook-Air.local>
2025-04-04 01:05:42 +0100

    removed auth

--- src/routes/pluginRoutes.js
+++ src/routes/pluginRoutes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const pluginController = require('../controllers/pluginController');
-const { authenticate } = require('../middleware/auth');
 
 // Get available plugins
-router.get('/servers/:serverId/plugins/available', authenticate, pluginController.getAvailablePlugins);
+router.get('/servers/:serverId/plugins/available', pluginController.getAvailablePlugins);
 
 // Get installed plugins for a server
-router.get('/servers/:serverId/plugins', authenticate, pluginController.getInstalledPlugins);
+router.get('/servers/:serverId/plugins', pluginController.getInstalledPlugins);
 
 // Install a plugin
-router.post('/servers/:serverId/plugins', authenticate, pluginController.installPlugin);
+router.post('/servers/:serverId/plugins', pluginController.installPlugin);
 
 // Uninstall a plugin
-router.delete('/servers/:serverId/plugins/:pluginId', authenticate, pluginController.uninstallPlugin);
+router.delete('/servers/:serverId/plugins/:pluginId', pluginController.uninstallPlugin);
 
 module.exports = router; 
